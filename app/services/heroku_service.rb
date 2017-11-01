a307a838f481c9a403bf79c8676b917c5df732ac
Simon Huber <simon.huber@renuo.ch>
2017-11-01 19:35:20 +0100

    connect with heroku

--- app/services/heroku_service.rb
+++ app/services/heroku_service.rb
@@ -6,12 +6,16 @@ class HerokuService
   end
 
   def create_app(name)
-    heroku.app.create(name: name)
+    @heroku.app.create(name: name)
   end
 
-  def create_build(name, tarball_url)
-    build = heroku.build.create(name, { source_blob: { url: tarball_url } })
+  def create_build(app_name, tarball_url)
+    build = @heroku.build.create(app_name, { source_blob: { url: tarball_url } })
     # TODO: handle case if failing and log it to somewhere
     build[:output_stream_url]
+  end
+
+  def create_log_session(app_name)
+    @heroku.log_session.create(app_name)
   end
 end
