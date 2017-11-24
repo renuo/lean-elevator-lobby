7c0551c4c68d2abf35718d8e6781627998e6a4c9
Josua Schmid <josua.schmid@renuo.ch>
2017-11-24 20:08:28 +0100

    Add apps to pipeline if configured

--- app/services/heroku_service.rb
+++ app/services/heroku_service.rb
@@ -1,10 +1,16 @@
 class HerokuService
   def initialize
-    @heroku = PlatformAPI.connect_oauth(ENV['HEROKU_AUTH'])
+    @region = 'eu'
+    @api_key = ENV['HEROKU_API_KEY']
+    @slave_pipeline_id = ENV['HEROKU_SLAVE_PIPELINE_ID']
+    @heroku = PlatformAPI.connect_oauth(@api_key)
   end
 
-  def create_app(name)
-    @heroku.app.create(name: name)
+  def create_app(app_name)
+    @heroku.app.create(name: app_name, region: @region).tap do
+      Rails.logger.debug "Heroku app created: #{app_name}"
+      add_to_pipeline(app_name) if @slave_pipeline_id.present?
+    end
   end
 
   def create_build(app_name, tarball_url)
@@ -16,5 +22,12 @@ class HerokuService
   def create_log_session(app_name)
     logs = @heroku.log_session.create(app_name)
     logs['logplex_url']
+  end
+
+  private
+
+  def add_to_pipeline(app_name)
+    @heroku.pipeline_coupling.create(app: app_name, pipeline: @slave_pipeline_id, stage: 'production')
+    Rails.logger.debug "Heroku app added to pipeline: #{app_name}"
   end
 end
