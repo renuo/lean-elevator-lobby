36100888b93412dcc637f2537a001b6a0debd273
Josua Schmid <josua.schmid@renuo.ch>
2017-11-24 17:31:00 +0100

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'])
 
 ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'].each do |team|
-  Team.create!(name: team, user: User.first, repository: "https://github.com/renuo/lean-elevators-#{team.downcase}.git")
+  user = User.create!(email: "#{team.downcase}@example.com", password: '12345678')
+  Team.create!(name: team, users: [user], repository: "https://github.com/renuo/lean-elevators-#{team.downcase}.git")
 end
 
 [3.seconds.ago, 2.seconds.ago, 1.second.ago].each do |time|
