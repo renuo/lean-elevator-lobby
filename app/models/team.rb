4ef160dc65c9ffc115f6a2ab4e11c9deb87bd634
Simon Huber <simon.huber@renuo.ch>
2017-11-27 17:46:00 +0100

    validates github

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,7 @@
 class Team < ApplicationRecord
   validates :repository, presence: true
+  validates_format_of :repository, with: /https?:\/\/(www\.)?github.com/i, message: 'Repository has to start with http://github.com'
+
   has_many :users
   has_one :decider_app
 end
