de14d6751fe33ed8911257fb80c28898e414520d
Josua Schmid <josua.schmid@renuo.ch>
2017-11-28 15:41:07 +0100

    Initial contribution

--- app/services/play_master_service.rb
+++ app/services/play_master_service.rb
@@ -1,36 +1,11 @@
 require 'lean_elevators'
 
 class PlayMasterService
-  def initialize(teams)
+  def initialize(teams, options = {})
     @teams = teams
     raise 'Not enough Heroku apps' unless @teams.all?(&:decider_app)
-  end
-
-  def setup
-    configure_rounds
-  end
-
-  def run
-    play_rounds
-  end
-
-  def persist_state(building)
-    round = Round.create!
-    BuildingState.create!(round: round,
-                          state_data: {
-                            elevators: building.elevators.map { |elevator|
-                              {
-                                floor_number: elevator.floor_number,
-                                people_carrying: elevator.people.count,
-                                people_transported: elevator.statistics
-                              }
-                            },
-                            floors: building.floors.map { |floor|
-                              {
-                                people_waiting: floor.people.count
-                              }
-                            }
-                          })
+    @options = default_setup_options.merge(options)
+    configure_business_logic
   end
 
   def play_rounds
@@ -40,15 +15,43 @@ class PlayMasterService
     end
   end
 
+  def default_setup_options
+    {
+      round_limit: 10,
+      building_size: 10,
+      decider_timeout: 0.3,
+      round_delay: 0
+    }
+  end
+
   private
 
-  def configure_rounds
+  def configure_business_logic
     LeanElevators.configure do |config|
-      config.building_size = 10
       config.net_deciders = @teams.map {|team| team.decider_app.dsn }
-      config.tick_limit = 100
-      config.decider_timeout = 0.3
-      config.round_delay = 0
+      config.building_size = @options[:building_size]
+      config.tick_limit = @options[:round_limit]
+      config.round_delay = @options[:round_delay]
+      config.decider_timeout = @options[:decider_timeout]
     end
+  end
+
+  def persist_state(building)
+    round = Round.create!
+    BuildingState.create!(round: round,
+                          state_data: {
+                              elevators: building.elevators.map { |elevator|
+                                {
+                                    floor_number: elevator.floor_number,
+                                    people_carrying: elevator.people.count,
+                                    people_transported: elevator.statistics
+                                }
+                              },
+                              floors: building.floors.map { |floor|
+                                {
+                                    people_waiting: floor.people.count
+                                }
+                              }
+                          })
   end
 end
