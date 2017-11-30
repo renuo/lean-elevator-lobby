9656beeb417c51a0ede9a503f113f6ebb1c06e83
Josua Schmid <josua.schmid@renuo.ch>
2017-11-30 13:29:03 +0100

    Refactor

--- app/jobs/game_job.rb
+++ app/jobs/game_job.rb
@@ -2,6 +2,6 @@ class GameJob < ApplicationJob
   queue_as :default
 
   def perform(options = {})
-    PlayMasterService.new(Team.select {|team| team.decider_app }, options)
+    PlayMasterService.new(Team.select(&:decider_app), options)
   end
 end
