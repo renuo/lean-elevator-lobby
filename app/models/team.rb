809f583d3c6e98349540aa78a55dd3b910c13341
Josua Schmid <josua.schmid@renuo.ch>
2017-12-01 13:54:11 +0100

    Add dependent option to references

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,8 @@ class Team < ApplicationRecord
   validates :repository, presence: true
   validates :repository, format: { with: /^[^\/].*(?<!\.git)$/i, message: 'Repository has to include only owner and name', multiline: true }
 
-  has_many :users
-  has_one :decider_app
+  has_many :users, dependent: :nullify
+  has_one :decider_app, dependent: :destroy
 
   def tarball_of_github_repo
     "#{extended_git_url}/archive/master.tar.gz"
