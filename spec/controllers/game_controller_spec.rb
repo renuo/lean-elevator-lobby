d2a3d3e4951c0af0e6fbbf455daf5d218de80103
Josua Schmid <josua.schmid@renuo.ch>
2017-12-01 13:48:21 +0100

    Make tests pass

--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -1,26 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe GameController, type: :controller do
+  login_user
 
   describe "GET #admin" do
     it "returns http success" do
-      get :admin
+      get :index
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #start" do
     it "returns http success" do
-      get :start
-      expect(response).to have_http_status(:success)
+      get :start, params: { start_with_game_config: {
+          building_size: 1,
+          decider_timeout: 0.1,
+          round_delay: 0,
+          round_limit: 0
+      }}
+      expect(response).to redirect_to game_index_path
     end
   end
 
   describe "GET #stop" do
     it "returns http success" do
       get :stop
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to game_index_path
     end
   end
-
 end
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,8 @@
+module ControllerMacros
+  def login_user(user = nil)
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in(user || create(:user))
+    end
+  end
+end
