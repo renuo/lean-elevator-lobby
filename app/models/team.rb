934a8c76b5e0bb2e77120ad3fa094376fb0d7981
Josua Schmid <josua.schmid@renuo.ch>
2017-12-01 14:14:41 +0100

    Create Rubocop Todo

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ApplicationRecord
   validates :repository, presence: true
-  validates :repository, format: { with: /^[^\/].*(?<!\.git)$/i, message: 'Repository has to include only owner and name', multiline: true }
+  validates :repository, format: { with: /^[^\/].*(?<!\.git)$/i,
+                                   message: 'Repository has to include only owner and name',
+                                   multiline: true }
 
   has_many :users, dependent: :nullify
   has_one :decider_app, dependent: :destroy
